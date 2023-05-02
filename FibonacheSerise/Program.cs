bbb9971de393f2c038767479392d333cac284659
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-02 16:40:55 +0530

    [Mayur:-Added Program.cs file]

---
+++ FibonacheSerise/Program.cs
@@ -0,0 +1,23 @@
+﻿using FibonacciSerise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacheSerise
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FibonacciSeries.FibonacciAndSeries();
+            PerfectNumber.FindPerfectNumber();
+            Reverse.ReverseNumber();
+            CheckPrime.CheckPrimeNumber();
+
+            Console.ReadKey();
+        }
+    }
+}
