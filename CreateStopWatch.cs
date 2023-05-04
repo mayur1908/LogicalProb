1b6a1b595352c2afe74c590200d008baca368a28
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-04 14:11:42 +0530

    [mayur added :- StopWatch]

---
+++ CreateStopWatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacheSerise
+{
+    public class CreateStopWatch
+    {
+        public void StopWatch()
+        {
+            Console.WriteLine("Press any key to start the stopwatch.");
+            Console.ReadKey();
+
+            DateTime startTime = DateTime.Now;
+
+            Console.WriteLine("Press any key to stop the stopwatch.");
+            Console.ReadKey();
+
+            DateTime endTime = DateTime.Now;
+
+            TimeSpan elapsedTime = endTime - startTime;
+
+            Console.WriteLine("Elapsed time: " + elapsedTime);
+        }
+    }
+}
